2a1a7ca05bad90c54634fc306716d0e1e2630340
RamKumarChaudhary <chaudhary8085@gmail.com>
2019-10-10 21:13:32 +0530

    test

--- app/src/main/java/com/daycare/AllPhotoActivity.java
+++ app/src/main/java/com/daycare/AllPhotoActivity.java
@@ -38,7 +38,6 @@ public class AllPhotoActivity extends AppCompatActivity {
     Toolbar toolbar;
     LinearLayout rl_open,rl_cancle,rl_delete,rl_share;
     boolean isDeleted=false;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
